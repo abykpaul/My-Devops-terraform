107cfb971dcd8d4a01526d227931749357013e5f
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-06-21 00:03:51 +0530

    Update main.tf

--- jenkins-aws-modules-terra/modules/iam/main.tf
+++ jenkins-aws-modules-terra/modules/iam/main.tf
@@ -15,6 +15,6 @@ data "aws_iam_policy_document" "ec2_assume_role" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-profile"
+  name = "ec2-profile-${var.env}"
   role = aws_iam_role.ec2_role.name
 }
