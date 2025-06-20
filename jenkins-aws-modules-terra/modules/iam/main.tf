6edbaa8ada21dd6139c9ba90d1a9137b96d122f1
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-06-20 23:59:49 +0530

    Update main.tf

--- jenkins-aws-modules-terra/modules/iam/main.tf
+++ jenkins-aws-modules-terra/modules/iam/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_iam_role" "ec2_role" {
-  name               = "ec2-role"
+  name               = "ec2-role-${var.env}" # dev, prod, etc.
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
